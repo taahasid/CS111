45456e2c61a45bd8e93bf99f0a0fbc1e9fdf3e2f
taahasid <83999365+taahasid@users.noreply.github.com>
2021-08-13 21:22:29 -0400

    OrderCheck.Java
    OrderCheck.java that takes four int command-line arguments w, x, y, and z. Define a boolean variable whose value is true if the four values are either in strictly ascending order (w < x < y < z) or strictly descending order (w > x > y > z), and false otherwise. Then, display the boolean variable value.

---
+++ OrderCheck.java
@@ -0,0 +1,35 @@
+import static java.lang.System.out;
+
+/*************************************************************************
+ *  Compilation:  javac OrderCheck.java
+ *  Execution:    java OrderCheck 5 10 15 2
+ *
+ *  @author: Taaha Siddiqui
+ *
+ *  Prints true if the four integer inputs are in strictly ascending
+ *  or descending order, false otherwise
+ *
+ *  % java OrderCheck 5 10 15 2
+ *  false
+ *
+ *  % java OrderCheck 15 11 9 4
+ *  true
+ *
+ *************************************************************************/
+public class OrderCheck {
+
+    public static void main(String[] args) {
+        if (args.length == 4) {
+            int w = Integer.parseInt(args[0]);
+            int x = Integer.parseInt(args[1]);
+            int y = Integer.parseInt(args[2]);
+            int z = Integer.parseInt(args[3]);
+
+            boolean result = (w < x && x < y && y < z) || (w > x && x > y && y > z);
+
+            System.out.println(result);
+        } else {
+            System.out.println("Please enter four integers as command line arguments");
+        }
+    }
+}
